141cdd8942c6df93f6fcf2b88a8c2a1e2ab1fc45
Victor Chan <victorcnk@gmail.com>
2021-01-24 12:26:59 +0800

    big o

---
+++ DSA-Big-O.js
@@ -0,0 +1,144 @@
+
+/*
+1. What is the Big O for this?
+1) Determine the Big O for the following algorithm: 
+You are sitting in a room with 15 people. 
+You want to find a playmate for your dog, preferably of the same breed. 
+So you want to know if anyone out of the 15 people have the same breed as your dog. 
+You stand up and yell out, who here has a golden retriever and would like to be a playdate for my golden. 
+omeone yells - "I do, be happy to bring him over"
+
+2) Determine the Big O for the following algorithm: 
+You are sitting in a room with 15 people. 
+You want to find a playmate for your dog who is of the same breed. 
+So you want to know if anyone out of the 15 people have the same breed as your dog. 
+You start with the first person and ask him if he has a golden retriever. 
+He says no, then you ask the next person, and the next, 
+and the next until you find someone who has a golden or there is no one else to ask.
+*/
+
+// 2. Even or odd
+
+function isEven(value) {
+    if (value % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// 3. Are you here?
+
+function areYouHere(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+}
+
+// 4. Doubler
+
+function doubleArrayValues(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= 2;
+    }
+    return array;
+}
+
+// 5. Naive search
+
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+
+// 6. Creating pairs:
+
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+// 7. Compute the sequence
+
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i === 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+
+// 8. An efficient search
+
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return 
+
+//9. Random element
+
+function findRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//10. What Am I?
+
+function isWhat(n) {
+    if (n < 2 || n % 1 !== 0) {
+        return false;
+    }
+    for (let i = 2; i < n; ++i) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+//11. Tower of Hanoi
+
+
+
+// 12. Iterative version
+
+
+// 13. Recursive Big O
+
+
+// 14. Iterative Big O
